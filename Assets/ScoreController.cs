56c41c5cd2699922486862f5afccdc669705194a
miorihashimoto <132665840+miorihashimoto@users.noreply.github.com>
2023-06-06 13:37:24 +0900

    Initial commit

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    private GameObject scoreText;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        this.scoreText.GetComponent<Text>().text = "Score "+score.ToString();
+
+    }
+
+       void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag=="SmallStarTag")
+        {
+           score += 100;
+        }
+
+        else if(other.gameObject.tag=="LargeStarTag")
+        {
+            score += 250;
+        }
+
+        else if(other.gameObject.tag=="SmallCloudTag")
+        {
+           score += 200;
+        }
+
+        else if(other.gameObject.tag=="LargeCloudTag")
+        {
+            score += 300;
+        }
+
+    }
+}
